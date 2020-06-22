e04a6697c8d1c43d32a07ff88300c097eb225192
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2020-06-22 15:04:02 -0500

    feat(bootstrap): add to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +16,9 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    TodoListModule
+    TodoListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
