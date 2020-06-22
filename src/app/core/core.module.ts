4a24e03f5f504872061a7367c6b65d5f3460b8c3
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2020-06-22 15:59:37 -0500

    feat(service): todo list service

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoListService } from './todo-list.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [TodoListService],
+})
+export class CoreModule {}
