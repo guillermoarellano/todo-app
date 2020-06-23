ee4f4a9bcd34511183c6eafad8a6a259f091b06b
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2020-06-23 07:35:44 -0500

    feat(add-todo): create service that notifies observables

---
+++ src/app/todo-page/add-todo/service/add-todo.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { TODOItem } from '@app/shared/models/todoitem';
+
+@Injectable()
+export class AddTodoService {
+  public get currentTODO(): TODOItem {
+    return this.CurrentTODO;
+  }
+  public set currentTODO(value: TODOItem) {
+    this.CurrentTODO = { ...value };
+  }
+
+  public todoItemEdit = new EventEmitter<TODOItem>();
+  public todoItemCreate = new EventEmitter<TODOItem>();
+  private CurrentTODO: TODOItem = ({title: '', text: ''}) as TODOItem;
+
+  public save(form: NgForm) {
+    if (!form.valid) {
+      console.log('Invalid form!');
+      return;
+    }
+
+    const todoToSave = {
+      ...this.currentTODO
+    };
+
+    if (todoToSave.id) {
+      this.todoItemEdit.next(todoToSave);
+      form.resetForm();
+    } else {
+      this.todoItemCreate.next(todoToSave);
+      form.resetForm();
+    }
+  }
+}
